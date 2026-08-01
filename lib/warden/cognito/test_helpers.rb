750796bb921faf9ea9575ae026cd8353b46f5550
Jacobo Bouzas Quiroga <jacobo.bouzas.external@factorial.co>
2026-08-01 02:22:59 +0200

    perf: make crypto key generation lazy
    We defer the initialization of the JWK crypto key, which might hold up systems while waiting for enough entropy to be available, but is never used by productive code.

--- lib/warden/cognito/test_helpers.rb
+++ lib/warden/cognito/test_helpers.rb
@@ -3,11 +3,11 @@ module Warden
     class TestHelpers
       class EnvironmentError < StandardError; end
 
-      @jwk = JWT::JWK.new(OpenSSL::PKey::RSA.new(2048))
-
       class << self
-        attr_reader :jwk
-
+        def jwk
+          @jwk ||= JWT::JWK.new(OpenSSL::PKey::RSA.new(2048))
+        end
+        
         def setup
           Warden::Cognito.config.jwk = { key: jwk, issuer: local_issuer }
         end
